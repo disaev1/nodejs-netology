7adef418ed926dc25c091e520574914eb1723488
Dan Isaev <dkikrlv2@mail.ru>
2021-12-25 22:45:11 +0300

    Fixed errors

--- 1.6-express/middleware.js
+++ 1.6-express/middleware.js
@@ -1,5 +1,5 @@
 const upload = require('./multer');
-const bookFileFields = ['fileCover', 'fileName', 'fileBook'];
+const { bookFileFields } = require('./utils');
 
 const uploadBookFileFields = upload.fields(bookFileFields.map(field => ({ name: field, maxCount: 1 })));
 
--- 1.6-express/utils.js
+++ 1.6-express/utils.js
@@ -1,25 +1,28 @@
+const _ = require('lodash');
 const bookFields = ['title', 'description', 'authors', 'favourite'];
+const bookFileFields = ['fileCover', 'fileName', 'fileBook'];
 
 function parseBookDataFromReq(req) {
   const res = _.pick(req.body, bookFields);
 
   // In form data "authors" field is a JSON string, it is not automatically parsed
   if ('authors' in res && typeof res.authors === 'string') {
-    res.authors = JSON.parse(res.authors);
+    try {
+      res.authors = JSON.parse(res.authors);
+    } catch (e) {
+      res.authors = [res.authors];
+    }
   }
 
   if (req.files) {
     bookFileFields.forEach(field => {
-      console.log('consider field', field, req.files[field]?.filename);
       if (req.files[field]) {
-        console.log('data is', req.files[field]);
         res[field] = req.files[field][0].filename;
       }
     });
   }
-  console.log('file is', req.file, 'files are', req.files, 'res is', res);
 
   return res;
 }
 
-module.exports = { parseBookDataFromReq };
+module.exports = { parseBookDataFromReq, bookFileFields };
