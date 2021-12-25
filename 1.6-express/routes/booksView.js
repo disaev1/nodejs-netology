fbcea356636b64595d60436841c2f11bbe6224f0
Dan Isaev <dkikrlv2@mail.ru>
2021-12-25 22:47:14 +0300

    EJS, fontawesome, updated styles and views, fixed books view routes

--- 1.6-express/routes/booksView.js
+++ 1.6-express/routes/booksView.js
@@ -1,39 +1,18 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
+
+const { parseBookDataFromReq } = require('../utils');
 const { NotFoundError } = require('../errors');
 const { uploadBookFileFields } = require('../middleware');
 
 const router = express.Router();
+const backToBooksLink = { to: '/books', title: 'К списку', icon: 'arrow-left' };
 
 let books = require('../books.json'); // TODO: No commit
 // let books = []; // TODO: Uncomment and commit
 
 router.get('/', (__, res) => {
-  res.render('books/index', { title: 'Главная', books });
-});
-
-router.get('/create', (__, res) => {
-  res.render('books/create', { title: 'Добавить книгу', book: {} });
-});
-
-router.post('/create', uploadBookFileFields, (req, res) => {
-  const newBook = parseBookDataFromReq(req);
-  books.push(newBook);
-  console.log('newBook is', newBook);
-
-  res.redirect('/');
-});
-
-router.get('/update/:id', (req, res) => {
-  const { id } = req.params;
-  const target = books.find(book => book.id === id);
-
-  if (target) {
-    res.render('books/create', { title: 'Редактировать книгу', book: target });
-
-    return;
-  }
-
-  throw new NotFoundError(`There is no book with id = ${id}!`);
+  res.render('books/index', { title: 'Главная', books, link: false });
 });
 
 router.get('/:id', (req, res) => {
@@ -41,7 +20,57 @@ router.get('/:id', (req, res) => {
   const target = books.find(book => book.id === id);
 
   if (target) {
-    res.render('books/view', { title: 'Информация о книге', book: target });
+    res.render('books/view', { title: 'Информация о книге', book: target, link: backToBooksLink });
+
+    return;
+  }
+
+  throw new NotFoundError(`There is no book with id = ${id}!`);
+});
+
+router.get('/create', (__, res) => {
+  res.render('books/create', { title: 'Добавить книгу', book: {}, link: backToBooksLink });
+});
+
+router.post('/create', uploadBookFileFields, (req, res) => {
+  const newBook = { id: uuid(), ...parseBookDataFromReq(req) };
+  
+  books.push(newBook);
+  res.redirect('/books');
+});
+
+router.post('/update/:id', uploadBookFileFields, (req, res) => {
+  const { id } = req.params;
+  const target = books.find(book => book.id === id);
+  console.log('update book', target);
+
+  if (!target) {
+    throw new NotFoundError(`There is no book with id = ${id}!`);
+  }
+
+  const updatedBook = { ...target, ...parseBookDataFromReq(req) };
+
+  books = books.map(book => {
+    if (updatedBook.id === book.id) {
+      return updatedBook;
+    }
+
+    return book;
+  });
+
+  res.redirect('/books');
+});
+
+router.get('/update/:id', (req, res) => {
+  const { id } = req.params;
+  const target = books.find(book => book.id === id);
+  console.log('edit book', target);
+
+  if (target) {
+    res.render(
+      'books/create',
+      { title: 'Редактировать книгу', book: target, link: backToBooksLink }
+    );
 
     return;
   }
