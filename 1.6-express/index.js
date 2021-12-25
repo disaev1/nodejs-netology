5a20d99bd25d70eb20cd8414c7a0fe023e30b01d
Dan Isaev <dkikrlv2@mail.ru>
2021-12-25 22:44:24 +0300

    public/ route for static files

--- 1.6-express/index.js
+++ 1.6-express/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const apiRoutes = require('./routes/api');
 const booksViewRoutes = require('./routes/booksView');
 
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/books', booksViewRoutes);
+app.use('/public', express.static(path.join(__dirname, './public')));
 
 app.use((req, res) => {
   res.status(404);
