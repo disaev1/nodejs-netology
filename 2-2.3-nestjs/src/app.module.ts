9062cd2e43c02b62d903ede8062ee0ab5401308a
Dan Isaev <dkikrlv2@mail.ru>
2022-06-07 01:52:46 +0300

    Added mongoose connection

--- 2-2.3-nestjs/src/app.module.ts
+++ 2-2.3-nestjs/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const MONGO_DB_URL: string = process.env.MONGO_DB_URL || 'mongodb://db:27017/test';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, MongooseModule.forRoot(MONGO_DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
