094af64e2ac02525b5c8d8f3c54b94c4d8c1f586
Dan Isaev <dkikrlv2@mail.ru>
2022-10-23 14:36:07 +0300

    Nest books app: small refactoring

--- 2-2.3-nestjs/src/books/books.module.ts
+++ 2-2.3-nestjs/src/books/books.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 import { BookSchema, Book } from './schemas/book.schema';
+
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])],
   controllers: [BooksController],
-  providers: [BooksService]
+  providers: [BooksService],
 })
 export class BooksModule {}
